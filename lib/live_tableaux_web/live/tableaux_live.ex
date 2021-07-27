e8a63eb3e7e22f3d2a2ff20b9a003457640c8ed5
Pietro Loffredi <pietro.loffredi@gmail.com>
2021-07-27 10:53:06 +0200

    solved updating issues

--- lib/live_tableaux_web/live/tableaux_live.ex
+++ lib/live_tableaux_web/live/tableaux_live.ex
@@ -3,12 +3,12 @@ defmodule LiveTableauxWeb.TableauxLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, sequent: "", results: %{})}
+    {:ok, assign(socket, sequent: "")}
   end
 
   @impl true
   def handle_event("suggest", %{"q" => sequent}, socket) do
-    {:noreply, assign(socket, results: validate(sequent), sequent: sequent)}
+    {:noreply, assign(socket, sequent: sequent)}
   end
 
 
@@ -52,40 +52,5 @@ defmodule LiveTableauxWeb.TableauxLive do
          }
        ]
      })}
-  end
-
-
-  @impl true
-  def handle_info("update-tree", socket) do
-    {:noreply,
-     push_event(socket, "updateResultTree", %{
-       name: "T[p|q]",
-       children: [
-         %{
-           name: "T[!p]",
-           children: [
-             %{
-               name: "F[q]",
-               children: [
-                 %{name: "T[p]", children: [%{name: "X"}]},
-                 %{name: "T[q]", children: [%{name: "X"}]}
-               ]
-             }
-           ]
-         }
-       ]
-     })}
-  end
-
-  defp validate(sequent) do
-    if not LiveTableauxWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, sequent) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
