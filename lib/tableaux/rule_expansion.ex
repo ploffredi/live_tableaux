34a4217724e2847a8a56c38fa606e712f2a01859
Pietro Loffredi <pietro.loffredi@gmail.com>
2021-08-02 12:27:03 +0200

    renamed add functions

--- lib/tableaux/rule_expansion.ex
+++ lib/tableaux/rule_expansion.ex
@@ -15,24 +15,24 @@ defmodule RuleExpansion do
   end
 
   def expand(tree, %RuleExpansion{rule_type: :beta, source_nid: nid, expanded_nodes: [left, right]}) do
-    add_beta_rules(tree, left, right, nid, false, [])
+    expand_beta(tree, left, right, nid, false, [])
   end
 
   def expand(tree, %RuleExpansion{rule_type: :alpha, source_nid: nid, expanded_nodes: nodes}) do
-    add_alpha_rules(tree, nodes, nid, false, [])
+    expand_alpha(tree, nodes, nid, false, [])
   end
 
-  @spec add_alpha_rules(nil | BinTree.t(), [TableauxNode.t()], nil | binary(), boolean(), [binary()]) ::
+  @spec expand_alpha(nil | BinTree.t(), [TableauxNode.t()], nil | binary(), boolean(), [binary()]) ::
           BinTree.t()
   @doc ~S"""
   Apply an alpha rules from tableaux to all the leaf nodes of a tree. The function is useful when you
   need to create the first tree after the sequent parsing
   """
-  def add_alpha_rules(nil, _list, _ancestor, _ancestor_found, _path) do
+  def expand_alpha(nil, _list, _ancestor, _ancestor_found, _path) do
     nil
   end
 
-  def add_alpha_rules(
+  def expand_alpha(
         %BinTree{value: %TableauxNode{nid: nid}=value, left: nil, right: nil} = tree,
         list,
         ancestor,
@@ -62,7 +62,7 @@ defmodule RuleExpansion do
     end
   end
 
-  def add_alpha_rules(
+  def expand_alpha(
         %BinTree{value: %TableauxNode{nid: nid}=value, left: left, right: right} = tree,
         list,
         ancestor,
@@ -82,16 +82,16 @@ defmodule RuleExpansion do
       %BinTree{
         tree
         | left:
-            add_alpha_rules(left, list, ancestor, ancestor_found || nid == ancestor, [tree | path]),
+            expand_alpha(left, list, ancestor, ancestor_found || nid == ancestor, [tree | path]),
           right:
-            add_alpha_rules(right, list, ancestor, ancestor_found || nid == ancestor, [
+            expand_alpha(right, list, ancestor, ancestor_found || nid == ancestor, [
               tree | path
             ])
       }
     end
   end
 
-  @spec add_beta_rules(BinTree.t(), nil | TableauxNode.t(), nil | TableauxNode.t(), binary(), boolean(), [
+  @spec expand_beta(BinTree.t(), nil | TableauxNode.t(), nil | TableauxNode.t(), binary(), boolean(), [
           BinTree.t()
         ]) ::
           BinTree.t()
@@ -99,9 +99,9 @@ defmodule RuleExpansion do
   Apply a beta rules from tableaux to all the leaf nodes of a tree.
   """
 
-  def add_beta_rules(nil, _, _, _, _, _), do: nil
+  def expand_beta(nil, _, _, _, _, _), do: nil
 
-  def add_beta_rules(
+  def expand_beta(
         %BinTree{value: %TableauxNode{nid: nid}=value, left: nil, right: nil} = tree,
         %TableauxNode{sign: lsign, string: lstr} = lnode,
         %TableauxNode{sign: rsign, string: rstr} = rnode,
@@ -133,7 +133,7 @@ defmodule RuleExpansion do
     end
   end
 
-  def add_beta_rules(
+  def expand_beta(
         %BinTree{value: %TableauxNode{nid: nid}=value, left: left, right: right} = tree,
         lexp,
         rexp,
@@ -153,11 +153,11 @@ defmodule RuleExpansion do
       %BinTree{
         tree
         | left:
-            add_beta_rules(left, lexp, rexp, ancestor, ancestor_found || nid == ancestor, [
+            expand_beta(left, lexp, rexp, ancestor, ancestor_found || nid == ancestor, [
               tree | path
             ]),
           right:
-            add_beta_rules(right, lexp, rexp, ancestor, ancestor_found || nid == ancestor, [
+            expand_beta(right, lexp, rexp, ancestor, ancestor_found || nid == ancestor, [
               tree | path
             ])
       }
