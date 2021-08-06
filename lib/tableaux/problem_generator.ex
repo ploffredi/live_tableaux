65ec684c013b12a75eb680d61cd4ff64c884a717
Pietro Loffredi <pietro.loffredi@gmail.com>
2021-08-06 13:03:46 +0200

    refactor of the problem generator

--- lib/tableaux/problem_generator.ex
+++ lib/tableaux/problem_generator.ex
@@ -1,17 +1,17 @@
 defmodule ProblemGenerator do
   def generate(:php, n) do
     "(" <> (0..n
-     |> Enum.map(fn p -> 0..(n - 1) |> Enum.map(fn b -> "p#{p+1}_#{b+1}" end) end)
+     |> Enum.map(fn i -> 0..(n - 1) |> Enum.map(fn j -> "p#{i+1}_#{j+1}" end) end)
      |> Enum.map(fn l -> round_bracket(l, "|") end)
      |> Enum.join("&")) <>
       ") |- " <>
       ((n - 1)..0
-       |> Enum.flat_map(fn c -> fac_list(n - 1, c, []) end)
+       |> Enum.flat_map(fn c -> fac_list(n - 1, c) end)
        |> Enum.join("|"))
       
   end
 
-  def fac_list(n, c, l) do
+  def fac_list(n, c) do
     0..n
     |> Enum.map(fn i -> (i + 1)..(n + 1) |> Enum.map(&{&1, n - c}) end)
     |> Enum.with_index(fn e, j -> Enum.map(e, fn {e1, e2} -> "(p#{j+1}_#{e2+1}&p#{e1+1}_#{e2+1})" end) end)
@@ -27,7 +27,7 @@ defmodule ProblemGenerator do
     p
   end
 
-  def round_bracket_rec([p | t] = l, op) do
+  def round_bracket_rec([p | t], op) do
     "(" <> round_bracket_rec(t, op) <> op <> p <> ")"
   end
 end
