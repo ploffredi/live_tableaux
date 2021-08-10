23239c77e077fae4a7b998fe62ed44522d672b7d
Pietro Loffredi <pietro.loffredi@gmail.com>
2021-08-10 08:36:29 +0200

    problem generator simplified

--- lib/tableaux/problem_generator.ex
+++ lib/tableaux/problem_generator.ex
@@ -1,35 +1,17 @@
 defmodule ProblemGenerator do
   def generate(:php, n) do
-    left =
-      0..n
-      |> Enum.map(fn i -> 0..(n - 1) |> Enum.map(fn j -> "p#{i + 1}_#{j + 1}" end) end)
-      |> Enum.map_join("&", fn l -> round_bracket(l, "|") end)
-      |> (&"(#{&1})").()
+    left_list(n) <> " |- " <> right_list(n)
+  end
 
-    right =
-      right_list(n)
-      |> Enum.join("|")
-
-    left <> " |- " <> right
+  def left_list(n) do
+    Enum.map_join(0..n, "&", fn i ->
+      Enum.map_join(0..(n - 1), "|", fn j -> "p#{i + 1}_#{j + 1}" end) |> (&"(#{&1})").()
+    end)
   end
 
   def right_list(n) do
     for(i <- 0..n, j <- 0..(n - 1), k <- 0..n, do: {i, j, k})
     |> Enum.filter(fn {i, _j, k} -> i < k end)
-    |> Enum.sort_by(fn {_i, j, _k} -> j end)
-    |> Enum.map(fn {i, j, k} -> "(p#{i + 1}_#{j + 1}&p#{k + 1}_#{j + 1})" end)
-  end
-
-  def round_bracket(l, op) do
-    Enum.reverse(l)
-    |> round_bracket_rec(op)
-  end
-
-  def round_bracket_rec([p], _) do
-    p
-  end
-
-  def round_bracket_rec([p | t], op) do
-    "(" <> round_bracket_rec(t, op) <> op <> p <> ")"
+    |> Enum.map_join("|", fn {i, j, k} -> "(p#{i + 1}_#{j + 1}&p#{k + 1}_#{j + 1})" end)
   end
 end
